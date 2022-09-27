aa76f08f0ac9d7116a34b182998998ffbacf0df7
MAXIMILIANO LEONELO FRANCESCONI <maxifni_fsa@live.com.ar>
2022-09-27 20:27:54 -0300

    se realizo el login completo en un solo archivo

--- admin/src/core/usuarioModel.py
+++ admin/src/core/usuarioModel.py
@@ -5,12 +5,31 @@ class Usuario(db.Model):
     
     __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    nombre_usaurio = db.Column(db.String(30))    
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(50))   
     email = db.Column(db.String(50))
-    nombre = db.Column(db.String(30))
-    apellido = db.Column(db.String(30))
-    password = db.Column(db.String(20))
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
     activo = db.Column(db.Integer) #0 moroso (no) - 1 activo (si)
     inserted_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now)
     created_at = db.Column(db.DateTime, default=datetime.now())
 
+    def __init__(
+            self, email=None, username=None, password=None, first_name=None, last_name=None
+    ):
+        self.email = ((email),)
+        self.username = ((username),)
+        self.password = ((password),)
+        self.first_name = ((first_name),)
+        self.last_name = ((last_name),)
+        self.activo = 1
+
+    def __repr__(self):
+        return "<user(username='%s', first_name='%s', last_name='%s' )>" % (
+            self.username,
+            self.first_name,
+            self.last_name,
+        )
+
+    def esta_activo(self):
+        return self.activo
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,17 @@
+from asyncore import read
 from cgitb import handler
 from logging import handlers
 from flask import Flask
-from flask import render_template, request, redirect , url_for 
+from flask import render_template, request, redirect , url_for, flash, session 
+from src.core.usuarioModel import Usuario
 from src.web.helpers import handlers
 from src.core.config import config
 from src.core import database
-from flask_sqlalchemy import sqlalchemy
+from src.core.database import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_session.__init__ import Session
+from os import error
+from werkzeug.security import generate_password_hash
 
 # def create_app(static_folder="static"):
 def create_app(env="development", static_folder="static"):
@@ -14,6 +20,10 @@ def create_app(env="development", static_folder="static"):
 
     # configuracion de la bd
     database.init_app(app)
+    app.secret_key= "holamundo"
+    # Server Side session
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
 
     # @app.get("/")
     # def home():
@@ -29,12 +39,33 @@ def create_app(env="development", static_folder="static"):
         if request.method == "POST":
             print(request.form['username'])
             print(request.form['password'])
-            return render_template("auth/login.html")
-        else:
-            return render_template("auth/login.html")
-       
+            username = request.form['username']
+            password = request.form['password']
+            session["username"] = username
+            session["password"] = password
+            user = Usuario.query.filter_by(username=username).first()
+            print(user)
+            if user:
+                 if user.password == password:
+                    return redirect(url_for("prueba_usuario", user=user))
+                 elif user.password != password:
+                     flash("el nombre de usuario o contraseña es incorrecto")
+            else:
+                 flash("el nombre de usuario o contraseña es incorrecto")
+                 return redirect(url_for("login"))
+    
+        return render_template("auth/login.html")
+    
+    @app.route("/prueba_usuario/")
+    def prueba_usuario():
+            return render_template("prueba_usuario.html")            
 
-
+    @app.route("/loguot")
+    def logout():
+            session.pop("username", None)
+            print("logout")
+            return redirect(url_for("login"))
+    
     app.register_error_handler(404, handlers.not_found_error)
 
     # @app.cli.command(name="resetdb")
