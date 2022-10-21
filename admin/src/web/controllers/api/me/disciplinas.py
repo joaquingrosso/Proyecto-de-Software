82635358cf988da551f4f1c904bbd395fddf6890
Agnelli Nicolas <nico_edlp@hotmail.es>
2022-10-20 23:35:47 -0300

    Api disciplinas del usuario

--- admin/src/web/controllers/api/me/disciplinas.py
+++ admin/src/web/controllers/api/me/disciplinas.py
@@ -5,13 +5,21 @@ from src.core.models.asociado_model import Asociado
 def mostrar_disciplinas_de_un_asociado(id):
    try:
         asociado_actual = Asociado.query.get(id)
+        disciplinas = asociado_actual.disciplinas
       #   disciplinas_del_asociado_actual = Asociado.tiene_disciplina((id))
    except:
         return jsonify({"error": "500 Internal server Error"}), 500
     
    if not asociado_actual:
         return jsonify({"error": "404 el id no existe"}), 404
-    
+   lista_disciplinas = []
+   for d in disciplinas:
+        disciplina = {
+          "name" : d.name,
+          "instructors" : d.instructors,  
+          "date_time" : d.date_time 
+         
+        }
    dic= { 
 
             "id" : asociado_actual.id,
@@ -20,8 +28,9 @@ def mostrar_disciplinas_de_un_asociado(id):
             "tipo de documento": asociado_actual.document_type,
             "documento" : asociado_actual.document,
             "genero" : asociado_actual.gender,
-            # "disciplinas" : asociado_actual.tiene_disciplinas(id)
-         }
+            "disciplinas" : disciplina
+           }
+         
     
    resp= {'datos y disciplinas del asociado':dic }
    return jsonify(resp), 200
