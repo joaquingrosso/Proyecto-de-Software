7bedfc54242b15a7b1c0ab9cbb18c58b54ca29de
Joaquin Grosso <joagrosso@hotmail.com>
2022-10-27 20:06:58 -0300

    descomentando log required

--- admin/src/web/controllers/index_controller.py
+++ admin/src/web/controllers/index_controller.py
@@ -12,7 +12,7 @@ def inicio():
     return render_template("inicio_privada.html") 
 
 
-#@login_required
+@login_required
 def gestion_usuarios(nombre=' ' ):
     
     config = Config.get_self(Config, 1)
@@ -22,7 +22,7 @@ def gestion_usuarios(nombre=' ' ):
         nombre = nombre
     return render_template("gestion_usuarios.html", user=usuariosActualesActivos ) 
 
-#@login_required
+@login_required
 def gestion_asociados(nombre=' ' ):
     config = Config.get_self(Config, 1)
     page = request.args.get('page', 1, type=int)
@@ -31,7 +31,7 @@ def gestion_asociados(nombre=' ' ):
         nombre = nombre
     return render_template("gestion_asociados.html", asoc = asociadosActuales)
 
-#@login_required
+@login_required
 def gestion_disciplinas(nombre=' '):
     config = Config.get_self(Config, 1)
     page = request.args.get('page', 1, type=int)
@@ -40,20 +40,20 @@ def gestion_disciplinas(nombre=' '):
         nombre = nombre
     return render_template("gestion_disciplinas.html", discip = disciplinasActualesActivas)
 
-#@login_required
+@login_required
 def pago_cuotas():
     config = Config.get_self(Config, 1)
     page = request.args.get('page', 1, type=int)
     lista_asociados = Asociado.list_asociados_pag(page,config.cant)
     return render_template("pago_cuotas.html", listaA = lista_asociados )
 
-#@login_required
+@login_required
 def configuracion():
 
     config = Config.get_self(Config, 1)
     return render_template("configuracion.html", config=config)
 
-#@login_required
+@login_required
 def ver_perfil():
     username = session['username']
     user = Usuario.query.filter_by(username=username).first()
