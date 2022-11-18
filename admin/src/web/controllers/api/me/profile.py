ee21d86d35bc06ef1b2765f4e7af5a099f45a529
MAXIMILIANO LEONELO FRANCESCONI <maxifni_fsa@live.com.ar>
2022-11-18 20:17:42 -0300

    se agrego el endpoint para mostrar el carnet_digital

--- admin/src/web/controllers/api/me/profile.py
+++ admin/src/web/controllers/api/me/profile.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from src.web.controllers.api.auth.login_jwt import token_required
-from src.core.models.usuario_model import Usuario
+from src.core.models.cuota_model import Cuota
 from src.core.models.asociado_model import Asociado
 
 @token_required
@@ -10,8 +10,8 @@ def mostrar_usuario(current_user):
    if not current_user:
         return jsonify({"error": "404 el id no existe"}), 404
    asociado = Asociado.get_asociado_by_id(current_user.asociado_id)
-   print(current_user)
-   print(asociado)
+   # print(current_user)
+   # print(asociado)
    if asociado is not None:
       dic= { 
             "user": current_user.username,
@@ -36,3 +36,35 @@ def mostrar_usuario(current_user):
          }
    resp= dic 
    return jsonify(resp), 200
+
+
+#carnet digital
+@token_required
+def carnet_digital(current_user):
+#    import pdb
+#    pdb.set_trace()
+   if not current_user:
+        return jsonify({"error": "404"}), 404
+   asociado = Asociado.get_asociado_by_id(current_user.asociado_id)
+   cuotas_pagas = Cuota.cuota_asociado(current_user.asociado_id)
+   # print(cuotas_pagas)
+   # print(current_user)
+   # print(asociado)
+   for cuota in cuotas_pagas:
+           c = {    
+                      "estado":   cuota.estado,                                                
+               }
+   if asociado is not None:
+      dic= { 
+            "nombre": current_user.first_name,
+            "apellido": current_user.last_name,
+            "numero_socio": asociado.id,
+            "document_type": asociado.document_type,
+            "document_number": asociado.document,
+            "fecha_de_alta": asociado.phone_number,
+            "estado":   cuota.estado
+            #"estado":   cuotas_pagas.validar_periodo_actual()
+         }
+   resp= dic 
+   return jsonify(resp), 200
+   
