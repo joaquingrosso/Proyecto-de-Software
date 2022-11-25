12e95bcc3a0f8ae44510e60c5d80b985ea431d5a
Agnelli Nicolas <nico_edlp@hotmail.es>
2022-11-25 16:01:57 -0300

    Se arreglo un problema con la API para mostrar cuotas impagas

--- admin/src/web/controllers/api/me/cuotas_impagas.py
+++ admin/src/web/controllers/api/me/cuotas_impagas.py
@@ -23,30 +23,38 @@ def mostrar_cuotas_impagas(current_user):
             "disciplinas": [],
             "total_a_pagar": None
         }
-        dic["disciplinas"].append(disciplina)
-        cuota = Cuota.get_cuotas_by_disciplina_asociado(
-            d.id, asociado_actual.id)
-        for c in cuota:
-            if c.estado == "No-Paga":
-                mes = c.periodo.split(" ")                
-                if dic_mes.get(mes[0]) <= mes_actual:
-                    if dia_actual >= 1 and dia_actual <= 10:
-                        cuota = {
-                            "periodo": c.periodo,
-                            "monto": c.monto,
-                            "estado":c.estado,
-                        }
-                        total+= c.monto
-                    else:
-                        recargo = (c.monto * recargo_cuota)/100
-                        cuota = {
-                            "periodo": c.periodo,
-                            "monto": c.monto + recargo,
-                            "estado":c.estado,
-                        }
-                        total+= c.monto + recargo
-                    dic["disciplinas"][index]["cuotas"].append(cuota)
-        index += 1
+        index = 0
+        if asociado_actual is None:
+            return jsonify({"error": "404 el Asociado no existe"}), 404
+        for d in asociado_actual.disciplinas:
+            disciplina = {
+                "nombre": d.name,
+                "cuotas": []
+            }
+            dic["disciplinas"].append(disciplina)
+            cuota = Cuota.get_cuotas_by_disciplina_asociado(
+                d.id, asociado_actual.id)
+            for c in cuota:
+                if c.estado == "No-Paga":
+                    mes = c.periodo.split(" ")                
+                    if dic_mes.get(mes[0]) <= mes_actual:
+                        if dia_actual >= 1 and dia_actual <= 10:
+                            cuota = {
+                                "periodo": c.periodo,
+                                "monto": c.monto,
+                                "estado":c.estado,
+                            }
+                            total+= c.monto
+                        else:
+                            recargo = (c.monto * recargo_cuota)/100
+                            cuota = {
+                                "periodo": c.periodo,
+                                "monto": c.monto + recargo,
+                                "estado":c.estado,
+                            }
+                            total+= c.monto + recargo
+                        dic["disciplinas"][index]["cuotas"].append(cuota)
+            index += 1
         dic["total_a_pagar"]= total + dic["monto_base"]
 
     except:
